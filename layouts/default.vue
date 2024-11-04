0cbcc3ec824bf3c7c07e815a04b49dcdd71261f8
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-11-05 00:33:35 +0200

    tes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,18 @@
 <template>
   <div :class="{ 'dark': isDarkMode }" :style="{ direction: locale === 'ar-AR' ? 'rtl' : 'ltr' }">
-    <div  class="mb-0 px-20  flex justify-between items-center py-6  sticky top-0 z-10 navbar"
-    >
-    <div class="flex items-center gap-4 md:hidden">
+    <div class="mb-0 px-20 flex justify-between items-center py-6 sticky top-0 z-10 navbar">
+      <div class="flex items-center gap-4 md:hidden">
         <Icon
           @click="toggleSidebar"
           name="iconamoon:menu-burger-horizontal"
-          class=" text-xl text-white cursor-pointer"
+          class="text-xl text-white cursor-pointer"
         />
       </div>
 
       <!-- الروابط في الشاشات الكبيرة -->
       <div class="links md:flex items-center gap-4 hidden">
         <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400 bg-white text-black" 
+          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
           to="/" 
           active-class="bg-cyan-700 text-yellow-400" 
           exact-active-class="bg-cyan-700 text-yellow-400"
@@ -22,52 +21,53 @@
         </NuxtLink>
       
         <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400  bg-white text-black" 
-          
+          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
+          to="/services"
         >
-        {{ $t('Services') }}
-      </NuxtLink>
+          {{ $t('Services') }}
+        </NuxtLink>
+        
         <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400 bg-white text-black" 
-          
+          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
+          to="/about"
           active-class="bg-cyan-700 text-yellow-400" 
           exact-active-class="bg-cyan-700 text-yellow-400"
         >
-{{$t('About us')}}
+          {{ $t('About us') }}
         </NuxtLink>
+        
         <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400 bg-white text-black" 
-          
+          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
+          to="/contact"
           active-class="bg-cyan-700 text-yellow-400" 
           exact-active-class="bg-cyan-700 text-yellow-400"
         >
-{{$t('Contact us')}}
+          {{ $t('Contact us') }}
         </NuxtLink>
-
-      
       </div>
-      <div class="flex items-center ">
+
+      <div class="flex items-center">
         <LanguageSwitcher />
-
-      <DarkModeToggle />
-      <div class="flex gap-8 mx-4">
-        <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400 bg-white text-black" 
+        <DarkModeToggle />
+        <div class="flex gap-8 mx-4">
+          <NuxtLink 
+            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
+            to="/signup"
+            active-class="bg-cyan-700 text-yellow-400" 
+            exact-active-class="bg-cyan-700 text-yellow-400"
+          >
+            {{ $t('Sign up') }}
+          </NuxtLink>
           
-          active-class="bg-cyan-700 text-yellow-400" 
-          exact-active-class="bg-cyan-700 text-yellow-400"
-        >
-{{$t('Sign up')}}
-        </NuxtLink>
-        <NuxtLink 
-          class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400 bg-white text-black" 
-          
-          active-class="bg-cyan-700 text-yellow-400" 
-          exact-active-class="bg-cyan-700 text-yellow-400"
-        >
-{{$t('Login')}}
-        </NuxtLink>
-      </div>
+          <NuxtLink 
+            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400 bg-white text-black" 
+            to="/login"
+            active-class="bg-cyan-700 text-yellow-400" 
+            exact-active-class="bg-cyan-700 text-yellow-400"
+          >
+            {{ $t('Login') }}
+          </NuxtLink>
+        </div>
       </div>
     </div>
 
@@ -76,7 +76,7 @@
       <div v-if="isSidebarOpen" class="bg-cyan-500 md:hidden border-t-2 border-cyan-600">
         <div class="flex flex-col p-4">
           <NuxtLink 
-            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400" 
+            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400" 
             to="/" 
             active-class="bg-cyan-700 text-yellow-400" 
             exact-active-class="bg-cyan-700 text-yellow-400"
@@ -85,28 +85,27 @@
           </NuxtLink>
           
           <NuxtLink 
-            :class="['hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400', 
-                     { 'bg-cyan-700 text-yellow-400': isYourNurseActive }]" 
+            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400" 
+            to="/services"
           >
             {{ $t('Services') }}
           </NuxtLink>
+          
           <NuxtLink 
-            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold  hover:text-yellow-400" 
+            class="hover:bg-cyan-700 px-4 py-2 rounded-lg delayed cursor-pointer font-bold hover:text-yellow-400" 
+            to="/contact"
             active-class="bg-cyan-700 text-yellow-400" 
             exact-active-class="bg-cyan-700 text-yellow-400"
           >
-          {{$t('Contact us')}}
-
+            {{ $t('Contact us') }}
           </NuxtLink>
-
-          
         </div>
       </div>
     </transition>
     
     <Loader v-if="loading" />
     <main class="px-0 mt-0">
-      <slot />
+      <slot @slot-loaded="loading = false" />
     </main>
   </div>
 </template>
@@ -130,6 +129,7 @@ onMounted(() => {
 onBeforeUnmount(() => {
   window.removeEventListener('scroll', handleScroll);
 });
+
 // التحكم في حالة الـ loading عند التنقل بين الصفحات
 router.beforeEach((to, from, next) => {
   loading.value = true;
@@ -146,17 +146,10 @@ onMounted(() => {
 });
 
 const isSidebarOpen = ref(false);
-const isDropdownOpen = ref(false);
-
 const toggleSidebar = () => {
   isSidebarOpen.value = !isSidebarOpen.value;
 }
 
-const toggleDropdown = () => {
-  isDropdownOpen.value = !isDropdownOpen.value;
-}
-
-// إغلاق القائمة عند النقر خارجها
 const closeDropdownOnClickOutside = (event) => {
   const dropdown = document.querySelector('.relative'); // عنصر القائمة
   if (dropdown && !dropdown.contains(event.target)) {
@@ -164,7 +157,6 @@ const closeDropdownOnClickOutside = (event) => {
   }
 };
 
-// إضافة وإزالة مستمع للنقر عند تشغيل وإيقاف المكون
 onMounted(() => {
   document.addEventListener('click', closeDropdownOnClickOutside);
 });
@@ -207,74 +199,9 @@ body.dark {
   transform: translateX(0);
 }
 
-.slide-enter {
-  opacity: 0;
-  transform: translateX(-20px);
-}
-
-.dark .p-menubar .p-menubar-root-list {
-  color: white;
-}
-
-.p-menuitem {
-  color: black;
-}
-
-.dark .p-menuitem {
-  color: white;
-}
-
-.p-menubar {
-  color: white;
-}
-
-.relative {
-  position: relative;
-}
-
-.absolute {
-  position: absolute;
-}
-
-.z-10 {
-  z-index: 10;
-}
-
-.bg-white {
-  background-color: white;
-}
-
-.text-black {
-  color: black;
-}
-
-.shadow-lg {
-  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
-}
-
-.rounded {
-  border-radius: 0.25rem;
-}
-
-.hover\:bg-gray-100:hover {
-  background-color: #f7fafc;
-}
-
-.w-full {
-  width: 100%;
-}
-
-.mt-2 {
-  margin-top: 0.5rem;
-}
-
-.right-0 {
-  right: 0;
-}
-
 .navbar {
   background-color: transparent;
-  transition: background-color 0.5s ease, box-shadow 0.5s ease; /* دمج الخصائص في سطر واحد */
+  transition: background-color 0.5s ease, box-shadow 0.5s ease;
   width: 100%;
   position: fixed;
   top: 0;
@@ -283,10 +210,8 @@ body.dark {
 
 .navbar.scrolled {
   background-color: aqua;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2); /* ظل خفيف */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
 }
-
-
 
 .hover\:bg-cyan-700:hover {
   background-color: #0aa8c2 !important;
@@ -307,5 +232,4 @@ body.dark {
 .cursor-pointer {
   cursor: pointer;
 }
-
 </style>
