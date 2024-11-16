6f969a981781041149244004178d5ceed23e7541
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-11-17 00:43:57 +0200

    t

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
     <div >
-        <h1 class="flex mx-4 md:mx-40 mb-2 gap-1 ">{{ $t('Welcome Daif, nice to see you in your shop') }}
+        <h1 class="flex mx-4 md:mx-40 mb-2 gap-1 text-black">{{ $t('Welcome Daif, nice to see you in your shop') }}
             <Icon class="text-orange-400 text-xl" name="material-symbols:waving-hand-sharp" />
         </h1>
        <div class="flex justify-end">
